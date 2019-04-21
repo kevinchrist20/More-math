adc63cba18e3ab6cf84622187210abcc7e93d833
codemonk227 <kevinamevor@outlook.com>
2019-04-21 15:30:39 +0300

    \Made changes to comb function in the Algebra class.

--- more_math/algebra.py
+++ more_math/algebra.py
@@ -8,17 +8,17 @@ class Algebra:
     def comb(n, r):
         """ Find the number of different combinations of 'r' identical items to be located in 'n' different places
          where n >= r. It is referred to as the combinatorial coefficient and is denoted by C = n! / (n - r)!r!. """
-        if n >= r:
-            if n < 0 or r < 0:
-                raise ValueError("Enter non-negative integer")
-            else:
-                if n == r:
-                    return 1
-                elif n == 0 or r == 0:
-                    return 1
-                else:
-                    diff_n_r = factorial(n - r)
-                    combination = factorial(n) / (diff_n_r * factorial(r))
-                    return combination
+
+        if n < 0 or r < 0:
+            raise ValueError("Enter non-negative integers")
         else:
-            raise ValueError("{0} should be >= {0}".format(n, r))
+            if n == r:
+                return 1
+            elif n == 0 or r == 0:
+                return 1
+            elif n <= r:
+                raise ValueError("n should be greater or equal to r")
+            else:
+                diff_n_r = factorial(n - r)
+                combination = factorial(n) / (diff_n_r * factorial(r))
+                return combination
