4cf44861d8e7b6665b41cbaceadaa9ac0114f66c
codemonk227 <kevinamevor@outlook.com>
2019-04-21 18:40:37 +0300

    Implemented permutation function in the Algebra class and made some bug fixes to the combination function

--- more_math/algebra.py
+++ more_math/algebra.py
@@ -6,19 +6,31 @@ class Algebra:
 
     @staticmethod
     def comb(n, r):
-        """ Find the number of different combinations of 'r' identical items to be located in 'n' different places
-         where n >= r. It is referred to as the combinatorial coefficient and is denoted by C = n! / (n - r)!r!. """
+        """ Returns the number of different combinations of 'r' identical items to be located in 'n' different places
+         where n >= r. """
 
         if n < 0 or r < 0:
             raise ValueError("Enter non-negative integers")
         else:
-            if n == r:
-                return 1
-            elif n == 0 or r == 0:
-                return 1
-            elif n <= r:
+            if n < r:
                 raise ValueError("n should be greater or equal to r")
+            elif r == 0 or n == r:
+                return 1
             else:
                 diff_n_r = factorial(n - r)
                 combination = factorial(n) / (diff_n_r * factorial(r))
                 return combination
+
+    @staticmethod
+    def perm(n, r):
+        """ Returns the number of ordered combinations of 'n' objects taken 'r' at a time."""
+
+        if n < 0 or r < 0:
+            raise ValueError("Enter non-negative integers")
+        else:
+            if n < r:
+                raise ValueError("n should be greater or equal to r")
+            else:
+                diff_n_r = factorial(n - r)
+                permutation = factorial(n) / diff_n_r
+                return permutation
